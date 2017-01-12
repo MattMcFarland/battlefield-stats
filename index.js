d9242e4c649d4f1530c8825b4f973e97faa8d7c5
Matt McFarland <contact@mattmcfarland.com>
2017-01-12 04:14:28 +0000

    fix broken reference to services

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ class BattlefieldStats {
   constructor(apiKey) {
     assert(apiKey, 'apiKey missing, please provide the value from the given TRN-Api-Key from Battlefield Tracker');
     this.Api = new Api(apiKey);
-    Api.Loadout = new Loadout(this.Api);
-    Api.Progression = new Progression(this.Api);
-    Api.Server = new Server(this.Api);
-    Api.Stats = new Stats(this.Api);
-    Api.Platforms = Platforms;
+    this.Loadout = new Loadout(this.Api);
+    this.Progression = new Progression(this.Api);
+    this.Server = new Server(this.Api);
+    this.Stats = new Stats(this.Api);
+    this.Platforms = Platforms;
   }
 }
 
